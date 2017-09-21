1ffa822a6f42e4366c87ea01b0306e7f0086472a
sarmst211 <samuel_armstrong@comcast.com>
2017-09-21 13:42:50 -0600

    Third Commit

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-calender',
@@ -8,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class CalenderComponent implements OnInit {
   date = new Date();
   totalDays = daysInMonth(this.date.getMonth(), this.date.getUTCFullYear())
-  constructor() { }
+  dates: number[] = Array(this.totalDays + 1).fill(0).map((x, i) => i);
+  weekDay = adjustDays(this.date.getMonth(), this.date.getUTCFullYear());
+  constructor() {
+    this.dates.shift();
+  }
 
   ngOnInit() {
 
@@ -16,4 +21,7 @@ export class CalenderComponent implements OnInit {
 }
 function daysInMonth(month: number, year: number ) {
   return new Date(year, month, 0).getDate();
+}
+function adjustDays(month: number, year: number ) {
+  return new Date(year, month, 1).getDay();
 }
